9a21b024045c151cbab5df23ad919ccb471fa9d3
vivek garg <vivek.garg02@infosys.com>
2025-09-16 22:45:36 +0530

    initial commit: main python module

---
+++ main.py
@@ -0,0 +1,12 @@
+a = int(input("enter a number: "))
+b = int(input("enter another number: "))
+
+
+
+def sum(c, d) :
+    return (c + d)
+
+
+sumOfNumber = sum(a, b)
+
+print("The sum of the two numbers is: ", sumOfNumber)
